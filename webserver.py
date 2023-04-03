3617cced4c9ba45cf97efc8faf1146abb89af90e
surrey-78 <surendhark240@gmail.com>
2023-04-03 15:55:33 +0000

    Success

---
+++ webserver.py
@@ -0,0 +1,24 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content="""
+<html>
+<head>
+<title>Django</title>
+</head>
+<body>
+
+<h1>Django</h1>
+<p>This is Web Application Framework written in python</p>
+
+</body>
+</html>"""
+class myhandler(BaseHTTPRequestHandler):
+     def do_GET(self):
+         print("request received")
+         self.send_response(200)
+         self.send_header('content-type','text/html; charset=utf-8')
+         self.end_headers()
+         self.wfile.write(content.encode())
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running....")
+httpd.serve_forever()
